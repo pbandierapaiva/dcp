b726d3fec168eb5eb249d00be42fade99f398eb0
P.Bandiera-Paiva <paiva@unifesp.br>
2024-11-25 08:07:52 -0300

    envia msg telegram

--- sonda.py
+++ sonda.py
@@ -7,6 +7,9 @@ from conexao import conexao
 from conexao import conexao, rootpw
 from pyghmi.ipmi import command as IPMI
 
+import requests
+from tabulate import tabulate
+
 from datetime import datetime
 import pytz
 
@@ -58,6 +61,64 @@ def main():
         except:
             print(insereCmd)
     db.commit()
+    verificaStatus()
     print("sonda concluída")
+
+def verificaStatus():
+    sql = """
+        SELECT 
+        DC,
+        ROUND(AVG(temp), 2) AS avg_temp,
+        SUM(CASE WHEN state = 0 THEN 1 ELSE 0 END) AS stateON,
+        SUM(CASE WHEN state = 1 THEN 1 ELSE 0 END) AS stateOFF,
+        rodada
+        FROM servidor_log
+        WHERE rodada = (
+        SELECT MAX(rodada) FROM servidor_log
+        )
+        GROUP BY DC;
+        """
+    db = DB()
+    db.cursor.execute(sql)
+    results = db.cursor.fetchall()
+    enviaTelegram(results)
+
+
+def enviaTelegram(jdata):
+    BOT_TOKEN = "7703319717:AAEAqzGt3JV0lopIR95lKs8ew_EVnBvP9Hw"
+    CHAT_ID = "5211765818"
+
+    table_data = []
+    for row in jdata:
+        table_data.append([
+            row['DC'],
+            round(float(row['avg_temp']), 2),
+            int(row['stateON']),
+            int(row['stateOFF']),
+            row['rodada'].strftime('%Y-%m-%d %H:%M'),
+        ])
+
+    # Format the table using tabulate
+    headers = ["DC", "Avg Temp", " ON", "OFF", "Rodada"]
+    table = tabulate(table_data, headers=headers, tablefmt="pretty")
+
+
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": f"```\n{table}\n```",
+        "parse_mode": "MarkdownV2",  # Use MarkdownV2 for monospaced text
+
+    }
+
+    response = requests.post(url, json=payload)
+
+    if response.status_code == 200:
+        print("Mensagem Telegram enviada")
+    else:
+        print(f"Falha de envio: {response.status_code}, {response.text}")
+
 if __name__ == '__main__':
     main()
+    # x=verificaStatus()
